2ad915cf20f7ca149139ff51a522d3d8ecd5509c
Brian Clark <beerye432@gmail.com>
2016-04-16 19:07:18 -0700

    added DB connection

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const path = require("path");
 
 var express = require('express');
 var handlebars = require('express-handlebars');
+var pg = require('pg');
 
 var app = express();
 
@@ -26,6 +27,18 @@ app.use(parser.body.json());
 
 // Routes
 app.get("/", router.index.view);
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('index', {results: result.rows} ); }
+    });
+  });
+})
 
 // Start Server
 http.createServer(app).listen(app.get("port"), function() {
