d59b848de2ae07f6610a9109ed20b04628033299
Brian Clark <beerye432@gmail.com>
2016-04-21 18:06:02 -0700

    first shot at saving sessions

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const path = require("path");
 var express = require('express');
 var handlebars = require('express-handlebars');
 var pg = require('pg');
-var sessions = require('express-session');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
 
 var app = express();
 
@@ -28,6 +29,16 @@ app.set("views", __dirname + "/views");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(parser.body.urlencoded({ extended: true }));
 app.use(parser.body.json());
+app.use(cookieParser());
+
+app.use(session({
+  resave: false,
+  saveUninitialized: false,
+  secret: 'qwerty'
+}));
+
+//session middleware
+
 
 // Routes
 app.get("/", router.index.view); 
@@ -63,7 +74,37 @@ app.get('/create', function(req, res){
 	})
 })
 
-app.post("/validatelogin", router.index.validatelogin);
+app.post("/validatelogin", function(req, res){
+
+  var name = req.body.name;
+
+  pg.connect(process.env.DATABASE_URL, function(err, client, done){
+
+    client.query("SELECT * FROM users WHERE name = '"+name+"';", function(err, results){
+
+      done();
+
+      if(err){
+        res.render('login', err);
+      }
+      else{
+        if(results.rows.length == 0){
+          res.render('login', {message: 'There is no user named ' + name + ' in the database'});
+        }
+        else{
+
+          req.session.regenerate(function(){
+            req.session.user = results.rows[0].name;
+            req.session.role = results.rows[0].role;
+          });
+
+          res.render('index');
+        }
+      }
+    });
+  });
+});
+
 app.post("/validate", router.index.validate);
 app.post("/addcategory", router.category.add);
 app.post("/addproduct", router.product.add);
