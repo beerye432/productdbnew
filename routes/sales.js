fe3127999f0c05de8df3c4e81962f19cb172b772
Brian Clark <beerye432@gmail.com>
2016-05-18 17:22:04 -0700

    fixed something

--- routes/sales.js
+++ routes/sales.js
@@ -214,7 +214,7 @@ function viewCustomers(req, res){
 						query = client.query("SELECT orders.user_id as user, products.id as product,"
 									 		+"SUM(CASE WHEN products.id = orders.product_id THEN orders.price ELSE 0 END) AS total"
 									 		+" FROM orders, products"
-									 		+" WHERE orders.user_id = "+user.id+" AND products.id IN (SELECT products.id FROM products ORDER BY name OFFSET "+req.session.col+" ROWS FETCH NEXT 10 ROWS ONLY)"
+									 		+" WHERE orders.user_id = "+user.id+" AND products.id IN (SELECT products.id FROM products ORDER BY name OFFSET "+req.session.col+" ROWS FETCH NEXT "+products.length+" ROWS ONLY)"
 									 		+" GROUP BY products.id, orders.user_id"
 									 		+" ORDER BY products.name ASC;");
 
