c4c706714d0544982aa6599444feae522e0b1c1b
Brian Clark <beerye432@gmail.com>
2016-05-19 03:29:56 -0700

    mismatch with handlebars info

--- routes/sales.js
+++ routes/sales.js
@@ -358,10 +358,10 @@ function viewCustomersTopK(req, res){
 									 		+"SUM(CASE WHEN products.id = orders.product_id THEN orders.price ELSE 0 END) AS total"
 									 		+" FROM orders, products"
 									 		+" WHERE orders.user_id = "+user.id
-									 		+" AND products.id IN"
-									 		+" (SELECT products.id FROM products, orders"
+									 		+" AND products.name IN"
+									 		+" (SELECT products.name FROM products, orders"
 									 		+" WHERE orders.product_id = products.id AND categories.id = products.category_id"
-									 		+" AND categories.name LIKE '%"+req.session.categoryFilter+"%' GROUP BY products.id"
+									 		+" AND categories.name LIKE '%"+req.session.categoryFilter+"%' GROUP BY products.name"
 									 		+" ORDER BY SUM(orders.price) DESC OFFSET "+req.session.col+" ROWS FETCH NEXT "+products.length+" ROWS ONLY)"
 									 		+" GROUP BY products.id, orders.user_id"
 									 		+" ORDER BY total DESC;");
