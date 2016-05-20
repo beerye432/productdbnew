afa80bc2ba32a2ffadb613e37d60c067632d240b
Brian Clark <beerye432@gmail.com>
2016-05-19 22:43:04 -0700

    ambigious query

--- routes/sales.js
+++ routes/sales.js
@@ -319,7 +319,7 @@ function viewCustomersTopK(req, res){
 				console.log("products: "+ products);
 
 				//get users and their totals
-				query = client.query("SELECT users.id as id, users.name as name, CASE WHEN users.id = orders.user_id THEN SUM(price) ELSE 0 END AS total"+
+				query = client.query("SELECT users.id as id, users.name as name, CASE WHEN users.id = orders.user_id THEN SUM(orders.price) ELSE 0 END AS total"+
 									" FROM users LEFT OUTER JOIN orders ON users.id = orders.user_id, categories, products"+
 									" WHERE products.id = orders.product_id AND products.category_id = categories.id AND categories.name LIKE '%"+req.session.categoryFilter+"%'"+
 									" GROUP BY users.id, users.name, orders.user_id"+
